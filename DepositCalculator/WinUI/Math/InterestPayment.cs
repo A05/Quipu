da022566f7d604230e8dfde938e55af09b463e6d
serhii.rudnitskyi <serhii.rudnitskyi@LT-SS-568812>
2023-05-20 08:16:32 +0300

    wip

---
+++ DepositCalculator/WinUI/Math/InterestPayment.cs
@@ -0,0 +1,12 @@
+﻿namespace Sx.Vx.Quipu.DepositCalculator.WinUIAppServices
+{
+    public enum InterestPayment
+    {
+        Unknown = 0,
+        EveryMonth,
+        EveryQuarter,
+        EveryYear,
+        AtTheEndOfTerm,
+        Capitalization
+    }
+}
