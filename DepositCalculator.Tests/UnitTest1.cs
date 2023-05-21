3e56a29cbd9c5db81dc6c2eb13b75116354932ce
serhii.rudnitskyi <serhii.rudnitskyi@LT-SS-568812>
2023-05-21 08:25:58 +0300

    wip

---
+++ DepositCalculator.Tests/UnitTest1.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+
+namespace Sx.Vx.Quipu.DepositCalculator
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+        }
+    }
+}
