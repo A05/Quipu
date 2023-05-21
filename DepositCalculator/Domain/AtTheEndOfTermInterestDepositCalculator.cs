e903f24d57e81bbe8b2062d8cb668c0c767d5bf6
serhii.rudnitskyi <serhii.rudnitskyi@LT-SS-568812>
2023-05-21 11:42:46 +0300

    wip

--- DepositCalculator/Domain/AtTheEndOfTermInterestDepositCalculator.cs
+++ DepositCalculator/Domain/AtTheEndOfTermInterestDepositCalculator.cs
@@ -68,32 +68,6 @@ namespace Sx.Vx.Quipu.Domain
         {
             return termInMonths;
         }
-
-        //protected override DepositIncomePlan CalculateImpl(decimal amount, int termInMonths, decimal interestRate, InterestPayment interestPayment)
-        //{
-        //    var now = DateTime.Now;
-        //    var termEnd = now.AddMonths(termInMonths);
-        //    var termInDays = (termEnd - now).Days;
-
-        //    var totalIncome = 0m;
-
-        //    var yearPeriodStart = now;
-        //    while (termInDays > 0)
-        //    {
-        //        var nextYearPeriodStart = yearPeriodStart.AddYears(1);
-        //        var yearPeriodInDays = (nextYearPeriodStart - yearPeriodStart).Days;
-        //        var termInPeriodInDays = termInDays > yearPeriodInDays ? yearPeriodInDays : termInDays;
-
-        //        var periodIncome = amount * (interestRate / 100m) * ((decimal) termInPeriodInDays / yearPeriodInDays);
-        //        periodIncome = Round(periodIncome);
-        //        totalIncome += periodIncome;
-
-        //        termInDays -= yearPeriodInDays;
-        //        yearPeriodStart = nextYearPeriodStart;
-        //    }
-
-        //    return new DepositIncomePlan(totalIncome, new[] { (termEnd, totalIncome) });
-        //}
     }
 
     public class EveryMonthInterestDepositCalculator : SimplePercentDepositCalculator
@@ -106,36 +80,6 @@ namespace Sx.Vx.Quipu.Domain
         {
             return 1;
         }
-
-        //protected override DepositIncomePlan CalculateImpl(decimal amount, int termInMonths, decimal interestRate, InterestPayment interestPayment)
-        //{
-        //    var now = DateTime.Now;
-        //    var termEnd = now.AddMonths(termInMonths);
-        //    var termInDays = (termEnd - now).Days;
-
-        //    var totalIncome = 0m;
-        //    var incomes = new List<(DateTime date, decimal income)>(capacity: termInMonths);
-
-        //    var periodStart = now;
-        //    while (termInDays > 0)
-        //    {
-        //        var nextPeriodStart = periodStart.AddMonths(1);
-        //        var periodInDays = (nextPeriodStart - periodStart).Days;
-        //        var termInPeriodInDays = termInDays > periodInDays ? periodInDays : termInDays;
-        //        var periodYearInDays = (new DateTime(periodStart.Year + 1, 1, 1) - new DateTime(periodStart.Year, 1, 1)).TotalDays;
-
-        //        var periodIncome = amount * (interestRate / 100m) * (termInPeriodInDays / (decimal)periodYearInDays);
-        //        periodIncome = Round(periodIncome);
-
-        //        totalIncome += periodIncome;
-        //        incomes.Add((nextPeriodStart, periodIncome));
-
-        //        termInDays -= periodInDays;
-        //        periodStart = nextPeriodStart;
-        //    }
-
-        //    return new DepositIncomePlan(totalIncome, incomes);
-        //}
     }
 
     public class EveryQuarterInterestDepositCalculator : SimplePercentDepositCalculator
@@ -148,36 +92,6 @@ namespace Sx.Vx.Quipu.Domain
         {
             return 3;
         }
-
-        //protected override DepositIncomePlan CalculateImpl(decimal amount, int termInMonths, decimal interestRate, InterestPayment interestPayment)
-        //{
-        //    var now = DateTime.Now;
-        //    var termEnd = now.AddMonths(termInMonths);
-        //    var termInDays = (termEnd - now).Days;
-
-        //    var totalIncome = 0m;
-        //    var incomes = new List<(DateTime date, decimal income)>(capacity: termInMonths);
-
-        //    var periodStart = now;
-        //    while (termInDays > 0)
-        //    {
-        //        var nextPeriodStart = periodStart.AddMonths(3);
-        //        var periodInDays = (nextPeriodStart - periodStart).Days;
-        //        var termInPeriodInDays = termInDays > periodInDays ? periodInDays : termInDays;
-        //        var periodYearInDays = (new DateTime(periodStart.Year + 1, 1, 1) - new DateTime(periodStart.Year, 1, 1)).TotalDays;
-
-        //        var periodIncome = amount * (interestRate / 100m) * (termInPeriodInDays / (decimal)periodYearInDays);
-        //        periodIncome = Round(periodIncome);
-
-        //        totalIncome += periodIncome;
-        //        incomes.Add((nextPeriodStart, periodIncome));
-
-        //        termInDays -= periodInDays;
-        //        periodStart = nextPeriodStart;
-        //    }
-
-        //    return new DepositIncomePlan(totalIncome, incomes);
-        //}
     }
 
     public class EveryYearInterestDepositCalculator : SimplePercentDepositCalculator
@@ -190,35 +104,5 @@ namespace Sx.Vx.Quipu.Domain
         {
             return 12;
         }
-
-        //protected override DepositIncomePlan CalculateImpl(decimal amount, int termInMonths, decimal interestRate, InterestPayment interestPayment)
-        //{
-        //    var now = DateTime.Now;
-        //    var termEnd = now.AddMonths(termInMonths);
-        //    var termInDays = (termEnd - now).Days;
-
-        //    var totalIncome = 0m;
-        //    var incomes = new List<(DateTime date, decimal income)>(capacity: termInMonths);
-
-        //    var periodStart = now;
-        //    while (termInDays > 0)
-        //    {
-        //        var nextPeriodStart = periodStart.AddYears(1);
-        //        var periodInDays = (nextPeriodStart - periodStart).Days;
-        //        var termInPeriodInDays = termInDays > periodInDays ? periodInDays : termInDays;
-        //        var periodYearInDays = (new DateTime(periodStart.Year + 1, 1, 1) - new DateTime(periodStart.Year, 1, 1)).TotalDays;
-
-        //        var periodIncome = amount * (interestRate / 100m) * (termInPeriodInDays / (decimal)periodYearInDays);
-        //        periodIncome = Round(periodIncome);
-
-        //        totalIncome += periodIncome;
-        //        incomes.Add((nextPeriodStart, periodIncome));
-
-        //        termInDays -= periodInDays;
-        //        periodStart = nextPeriodStart;
-        //    }
-
-        //    return new DepositIncomePlan(totalIncome, incomes);
-        //}
     }
 }
