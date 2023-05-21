54aeca4ce9d9090330fa9a04bee0f81108389f22
serhii.rudnitskyi <serhii.rudnitskyi@LT-SS-568812>
2023-05-21 16:09:37 +0300

    wip

--- Domain/Calculators/ComplexPercentDepositCalculator.cs
+++ Domain/Calculators/ComplexPercentDepositCalculator.cs
@@ -100,6 +100,25 @@ namespace Sx.Vx.Quipu.Domain.Calculators
         {
         }
 
+        protected override DepositIncomePlan CalculateImpl(decimal amount, int termInMonths, decimal interestRate, InterestPayment interestPayment)
+        {
+            if (amount <= 0)
+                throw new ArgumentException();
+
+            var termStart = DateTime.Now;
+            var termEnd = termStart.AddMonths(termInMonths);
+
+            var dAmount = (double)amount;
+            var dInterestRate = (double)interestRate / 100d;
+            var dInterestRateByMonth = dInterestRate / 12;
+            var revenue = (decimal)(dAmount * Math.Pow(1 + dInterestRateByMonth, termInMonths));
+
+            var totalIncome = Round(revenue - amount);
+            var incomes = new[] { (termEnd, totalIncome) };
+
+            return new DepositIncomePlan(totalIncome, incomes.AsEnumerable());
+        }
+
         protected override int GetCapitalizationUnitCountPerYear() => 12;
 
         protected override int GetCapitalizationUnitCount(int termInMonths) => termInMonths;
