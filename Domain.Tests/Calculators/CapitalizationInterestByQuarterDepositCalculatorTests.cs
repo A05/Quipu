4a811e1baa41c318a8bdddf94ac5c4c5ae35fb57
serhii.rudnitskyi <serhii.rudnitskyi@LT-SS-568812>
2023-05-21 16:24:31 +0300

    wip

---
+++ Domain.Tests/Calculators/CapitalizationInterestByQuarterDepositCalculatorTests.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System.Linq;
+
+namespace Sx.Vx.Quipu.Domain.Calculators
+{
+    [TestClass]
+    public class CapitalizationInterestByQuarterDepositCalculatorTests
+    {
+        [TestMethod]
+        public void ShouldCalculate()
+        {
+            var sut = new CapitalizationInterestByQuarterDepositCalculator(null);
+
+            var plan = sut.Calculate(350000m, 9, 4.7m, InterestPayment.CapitalizationByQuarter);
+
+            Assert.IsNotNull(plan);
+            Assert.AreEqual(1, plan.Incomes.Count());
+            Assert.AreEqual(12483.03m, plan.TotalIncome);
+        }
+    }
+}
