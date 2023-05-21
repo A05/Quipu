2c8def317b1d0a3a2ef9b0f92f40cf2611b718b0
serhii.rudnitskyi <serhii.rudnitskyi@LT-SS-568812>
2023-05-21 09:33:25 +0300

    wip

--- DepositCalculator.Tests/Domain/EveryYearInterestDepositCalculatorTests.cs
+++ DepositCalculator.Tests/Domain/EveryYearInterestDepositCalculatorTests.cs
@@ -1,4 +1,5 @@
 ﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System.Linq;
 
 namespace Sx.Vx.Quipu.Domain
 {
@@ -6,8 +7,21 @@ namespace Sx.Vx.Quipu.Domain
     public class EveryYearInterestDepositCalculatorTests
     {
         [TestMethod]
-        public void TestMethod1()
+        public void ShouldCalculate()
         {
+            var sut = new EveryYearInterestDepositCalculator(null);
+
+            var plan = sut.Calculate(100, 12, 48m, InterestPayment.EveryYear);
+
+            Assert.IsNotNull(plan);
+            Assert.AreEqual(1, plan.Incomes.Count());
+            Assert.AreEqual(48.13m, plan.TotalIncome);
+
+            plan = sut.Calculate(2548, 19, 3m, InterestPayment.EveryYear);
+
+            Assert.IsNotNull(plan);
+            Assert.AreEqual(2, plan.Incomes.Count());
+            Assert.AreEqual(121.33m, plan.TotalIncome);
         }
     }
 }
