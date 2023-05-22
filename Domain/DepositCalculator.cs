1b17efebc61d9e49977f80a69043ff97ecbfa513
serhii.rudnitskyi <serhii.rudnitskyi@LT-SS-568812>
2023-05-22 09:02:05 +0300

    Move domain error messages into resources.

--- Domain/DepositCalculator.cs
+++ Domain/DepositCalculator.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using Sx.Vx.Quipu.Domain.Properties;
+using System;
 
 namespace Sx.Vx.Quipu.Domain
 {
@@ -12,22 +13,22 @@ namespace Sx.Vx.Quipu.Domain
             _next = next;
             
             if ((_interestPayment = interestPayment) == InterestPayment.Unknown)
-                throw new ArgumentException("Interest payment must be specified.", nameof(interestPayment));
+                throw new ArgumentException(ErrorMessages.InterestPaymentMustBeSpecified, nameof(interestPayment));
         }
 
         public DepositIncomePlan Calculate(Money money, int termInMonths, decimal interestRate, InterestPayment interestPayment)
         {
             if (money <= 0)
-                throw new ArgumentException("Amount must be greater than zero.", nameof(money));
+                throw new ArgumentException(ErrorMessages.AmountMustBeGreaterThanZero, nameof(money));
 
             if (termInMonths <= 0)
-                throw new ArgumentException("Term must be greater than zero.", nameof(termInMonths));
+                throw new ArgumentException(ErrorMessages.TermMustBeGreaterThanZero, nameof(termInMonths));
 
             if (interestRate <= 0)
-                throw new ArgumentException("Interest rate must be greater than zero.", nameof(interestRate));
+                throw new ArgumentException(ErrorMessages.InterestRateMustBeGreaterThanZero, nameof(interestRate));
 
             if (interestPayment == InterestPayment.Unknown)
-                throw new ArgumentException("Interest payment be specified.", nameof(interestPayment));
+                throw new ArgumentException(ErrorMessages.InterestPaymentMustBeSpecified, nameof(interestPayment));
 
             if (_interestPayment == interestPayment)
                 return CalculateImpl(money, termInMonths, interestRate, interestPayment);
@@ -35,7 +36,7 @@ namespace Sx.Vx.Quipu.Domain
             if (_next != null)
                 return _next.Calculate(money, termInMonths, interestRate, interestPayment);
 
-            throw new NotSupportedException($"The {interestPayment:G} interest payment is not supported yet.");
+            throw new NotSupportedException(string.Format(ErrorMessages.InterestPaymentIsNotSupportedYet, interestPayment));
         }
 
         protected abstract DepositIncomePlan CalculateImpl(Money amount, int termInMonths, decimal interestRate, InterestPayment interestPayment);
