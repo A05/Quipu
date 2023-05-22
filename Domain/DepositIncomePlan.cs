6e85f241c348292aaa9dc3a346d2811ede22c916
serhii.rudnitskyi <serhii.rudnitskyi@LT-SS-568812>
2023-05-22 09:06:21 +0300

    Add ToString method into DepositIncomePlan class.

--- Domain/DepositIncomePlan.cs
+++ Domain/DepositIncomePlan.cs
@@ -11,13 +11,18 @@ namespace Sx.Vx.Quipu.Domain
 
         public IEnumerable<(DateTime date, Money income)> Incomes { get; }
 
-        public DepositIncomePlan(Money totalIncome, IEnumerable<(DateTime date, Money income)> incomes)
+        internal DepositIncomePlan(Money totalIncome, IEnumerable<(DateTime date, Money income)> incomes)
         {
             TotalIncome = totalIncome;
 
             Incomes = incomes ?? throw new ArgumentNullException(nameof(incomes));
 
             Debug.Assert(TotalIncome == Incomes.Sum(i => i.income));
+        }
+
+        public override string ToString()
+        {
+            return $"{TotalIncome}, Incomes Count = {Incomes.Count()}";
         }
     }
 }
