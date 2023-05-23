b88d16a51921a043f1319da56a9ff3ba31f6d2bb
serhii.rudnitskyi <serhii.rudnitskyi@LT-SS-568812>
2023-05-23 18:49:48 +0300

    Adjust assemble info & split versions for UI & Domain modules.

--- DepositCalculator.Tests/Properties/AssemblyInfo.cs
+++ DepositCalculator.Tests/Properties/AssemblyInfo.cs
@@ -1,20 +1,9 @@
 using System.Reflection;
-using System.Runtime.CompilerServices;
 using System.Runtime.InteropServices;
 
-[assembly: AssemblyTitle("DepositCalculator.Tests")]
+[assembly: AssemblyTitle("Sx.Vx.Quipu.DepositCalculator.Tests")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("BL")]
-[assembly: AssemblyProduct("DepositCalculator.Tests")]
-[assembly: AssemblyCopyright("Copyright © BL 2023")]
-[assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
-[assembly: ComVisible(false)]
-
 [assembly: Guid("bdbcc86e-3201-4a7f-8ca4-e75dba13194b")]
-
-// [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("1.0.0.0")]
-[assembly: AssemblyFileVersion("1.0.0.0")]
